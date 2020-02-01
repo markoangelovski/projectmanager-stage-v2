abc30b10e3f862c2a1d2b2d72882be4da3a29561
Agelovski <you@example.com>
2020-02-01 11:02:33 +0100

    Prod deploy script

---
+++ src/deploy/prod.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const ghpages = require("gh-pages");
+
+const build = path.join(__dirname, "../../", "/build");
+const prod = path.join(__dirname, "../../", "/prod");
+
+// Rename React's "build" folder to "prod" for GHPages
+if (fs.existsSync(build) && !fs.existsSync(prod)) {
+  fs.renameSync(build, prod);
+}
+
+// Deploy /prod to gh-pages
+ghpages.publish(
+  prod,
+  {
+    remote: "origin-production",
+    user: {
+      name: "Angelovski",
+      email: "marko.angelovski@gmail.com"
+    }
+  },
+  err => {
+    if (err) throw err;
+    console.log(
+      "Deploy to https://markoangelovski.github.io/projectmanager-prod-v2 completed."
+    );
+  }
+);
